d8cad29d2a6bbe915000762f50c49ff288880608
giuppycode <giuseppecattolico2004@gmail.com>
2026-06-29 13:55:40 +0200

    chore(config): add Room, DataStore, KSP and ZXing dependencies
    - Add version catalog entries for room 2.8.4, datastore 1.2.1, ksp 2.0.21-1.0.28 and zxing 3.5.4
    - Apply KSP plugin at root level
    - Enable android.disallowKotlinSourceSets for AGP 9.x compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    alias(libs.plugins.ksp) apply false
 }
